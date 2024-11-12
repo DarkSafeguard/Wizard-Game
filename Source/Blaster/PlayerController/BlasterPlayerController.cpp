f942b7f8bafac56a639f76e885b17a9b64f1b1af
Adam Percival <imhank@me.com>
2024-11-12 10:40:44 -0800

    Implemented fix for seamless travel load errors.

--- Source/Blaster/PlayerController/BlasterPlayerController.cpp
+++ Source/Blaster/PlayerController/BlasterPlayerController.cpp
@@ -8,6 +8,8 @@
 #include "Components/TextBlock.h"
 #include "Blaster/Public/Character/BlasterCharacter.h"
 #include "Net/UnrealNetwork.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameMapsSettings.h"
 #include "Blaster/GameMode/BlasterGameMode.h"
 #include "Kismet/GameplayStatics.h"
 #include "Blaster/BlasterComponents/CombatComponent.h"
@@ -136,6 +138,65 @@ void ABlasterPlayerController::SetHUDBlueTeamScore(int32 BlueScore)
 		BlasterHUD->CharacterOverlay->BlueTeamScore->SetText(FText::FromString(ScoreText));
 	}
 }
+
+//Client SeamlessTravel Fix
+
+void ABlasterPlayerController::NotifyLoadedWorld(FName WorldPackageName, bool bFinalDest)
+{
+	Super::NotifyLoadedWorld(WorldPackageName, bFinalDest);
+
+	// Call custom ServerNotifyLoadedWorld
+	ServerNotifyLoadedWorldWorkaround(WorldPackageName);
+}
+
+bool ABlasterPlayerController::ServerNotifyLoadedWorldWorkaround_Validate(FName WorldPackageName)
+{
+	RPC_VALIDATE(WorldPackageName.IsValid());
+	return true;
+}
+
+void ABlasterPlayerController::ServerNotifyLoadedWorldWorkaround_Implementation(FName WorldPackageName)
+{
+	UE_LOG(LogPlayerController, Verbose, TEXT("ABlasterPlayerController::ServerNotifyLoadedWorldWorkaround_Implementation: Client loaded %s"), *WorldPackageName.ToString());
+
+	UWorld* CurWorld = GetWorld();
+
+	// Only valid for calling, for PC's in the process of seamless traveling
+	// NOTE: SeamlessTravelCount tracks client seamless travel, through the serverside gameplay code; this should not be replaced.
+	if (CurWorld != NULL && !CurWorld->IsNetMode(NM_Client) && SeamlessTravelCount > 0 && LastCompletedSeamlessTravelCount < SeamlessTravelCount)
+	{
+		// Update our info on what world the client is in
+		UNetConnection* const Connection = Cast<UNetConnection>(Player);
+
+		if (Connection != NULL)
+		{
+			Connection->SetClientWorldPackageName(WorldPackageName);
+		}
+
+		// if both the server and this client have completed the transition, handle it
+		FSeamlessTravelHandler& SeamlessTravelHandler = GEngine->SeamlessTravelHandlerForWorld(CurWorld);
+		AGameModeBase* CurGameMode = CurWorld->GetAuthGameMode();
+
+		if (!SeamlessTravelHandler.IsInTransition() && WorldPackageName == CurWorld->GetOutermost()->GetFName() && CurGameMode != NULL)
+		{
+			AController* TravelPlayer = this;
+			CurGameMode->HandleSeamlessTravelPlayer(TravelPlayer);
+		}
+		else
+		{
+			// This is the seamless travel fix for client loading before server and softlocking
+			// Skip this for TransitionMapName
+			FString TransitionMapString = GetDefault<UGameMapsSettings>()->TransitionMap.GetLongPackageName();
+			FName TransitionMapName(*TransitionMapString);
+			if (TransitionMapName != WorldPackageName)
+			{
+				LastCompletedSeamlessTravelCount = SeamlessTravelCount;
+			}
+		}
+	}
+}
+
+//End Fix
 
 void ABlasterPlayerController::Tick(float DeltaTime)
 {
