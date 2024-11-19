e6b7b8fb46ac7ae349f3895bdf63e3cf300a1235
Adam Percival <imhank@me.com>
2024-11-19 13:05:13 -0800

    Created Test Game Mode.

---
+++ Source/Blaster/GameMode/TestGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LobbyGameMode.h"
+#include "TestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ATestGameMode : public ALobbyGameMode
+{
+	GENERATED_BODY()
+	
+};
